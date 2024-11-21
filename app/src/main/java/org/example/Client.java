59584dc06216873c024b279321f89d20f3e0bec6
osakaruhub <teddyfreddy10@gmail.com>
2024-11-21 03:45:10 +0100

    Request/StatusCode enums work now

--- app/src/main/java/org/example/Client.java
+++ app/src/main/java/org/example/Client.java
@@ -1,5 +1,6 @@
 package org.example;
 
+
 public class Client {
     private static final int port = 1234;
     private static final String host = "localhost";
@@ -20,7 +21,7 @@ public class Client {
 
         while (socket.read() != ACK) {}
         String status = socket.read();
-        return Status
+        return new StatusCode(Integer.parseInt(status.substring(0, 3)), status.substring(4));
     }
 
     public static void main(String[] args) {
--- app/src/main/java/org/example/Requests.java
+++ app/src/main/java/org/example/Requests.java
@@ -2,19 +2,29 @@ package main.java.org.example;
 
 /**
  * Requests
- */
+ */ 
+// Statuscodes for the protocol. Use these to send preapplied or custom messages to the client. theres probably a better way, i just found it to work.
 public enum StatusCode {
     OK(1, "Ok"),
-    OCCUPIED(404, "Not Found"),
+    NOT_FOUND(404, "Not Found"),
     SERVER_ERROR(500, "Internal Server Error"),
     BAD_REQUEST(400, "Bad Request");
 
     private final int code;
-    private final String message;
+    private final String msg;
 
-    StatusCode(int code, String message) {
+    StatusCode(int code, String msg) {
         this.code = code;
-        this.message = message;
+        this.msg = msg;
+    }
+
+    public static StatusCode SEND(String msg) {
+        return new StatusCode(2, msg);
+    }
+
+    @Override
+    public String toString() {
+        return this.code + " " + this.msg;
     }
 
     public int getCode() {
@@ -26,9 +36,21 @@ public enum StatusCode {
     }
 }
 
+// Requestcodes for the protocol. Use these to send requests to the server, which decodes to what the client wants.
 public enum Request {
     ADD("200"),
     DELETE("201"),
     GET("202"),
-    SYNC("203"),
+    SYNC("203");
+
+    private final String code;
+
+    Request(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return this.code;
+    }
 }
