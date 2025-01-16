aa88be03cc6eb817bbd56eed21a71dabfcd0f232
Kur9ix <felixtobiaswalter@gmail.com>
2025-01-16 19:02:59 +0100

    commit

--- app/src/main/java/mtd/tasker/protocol/Request.java
+++ app/src/main/java/mtd/tasker/protocol/Request.java
@@ -1,47 +1,58 @@
 package mtd.tasker.protocol;
 
+import java.awt.Event;
 import java.io.Serializable;
 import mtd.tasker.protocol.InvalidCommandException;
 
 /**
- * Class for Request for the socket. A Request consists of a RequestCode and its content.
+ * Class for Request for the socket. A Request consists of a RequestCode and its
+ * content.
  *
  */
-public class Request implements Serializable{
+public class Request implements Serializable {
 
     private static final long serialVersionUID = 1L; // Recommended for Serializable classes
     private RequestCode code;
     private String content;
 
- /*
- * Creates a Request with a message attached
- * @param code the StatusCode
- */
+    /*
+     * Creates a Request with a message attached
+     * 
+     * @param code the StatusCode
+     */
     public Request(RequestCode code, String content) {
         this.code = code;
         this.content = content;
     }
 
- /*
- * Creates a Request with the default message for the request
- * @param code the StatusCode
- */
+    /*
+     * Creates a Request with the default message for the request
+     * 
+     * @param code the StatusCode
+     */
     public Request(RequestCode code) {
         this.code = code;
     }
 
- /*
-   gets the content inside the Response
- * @return code the StatusCode
- * */
+    public Request(RequestCode code, Event event) {
+        this.code = code;
+        this.content = event.getPerson() + ":" + event.getDate().toString() + ":" + event.getTag();
+    }
+
+    /*
+     * gets the content inside the Response
+     * 
+     * @return code the StatusCode
+     */
     public String getContent() {
         return this.content;
     }
 
- /*
-   gets the StatusCode for the Response
- * @return code the StatusCode
- * */
+    /*
+     * gets the StatusCode for the Response
+     * 
+     * @return code the StatusCode
+     */
     public String getCode() {
         return this.code.toString();
     }
@@ -50,9 +61,9 @@ public class Request implements Serializable{
         return this.code;
     }
 
-@Override
-public String toString() {
-    return code + " " + content;
-}
+    @Override
+    public String toString() {
+        return code + " " + content;
+    }
 
 }
